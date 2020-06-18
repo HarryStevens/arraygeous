8c2b695e9f6371d482fb637cd8f81c3340cc1d7a
HarryStevens <harryjosephstevens@gmail.com>
2020-06-18 15:56:39 -0400

    Fix typo

--- test/mean-test.js
+++ test/mean-test.js
@@ -1,7 +1,7 @@
 const tape = require("tape"),
     arr = require("../");
 
-tape("mean returns the mean of an array according to an option accessor function", test => {
+tape("mean returns the mean of an array according to an optional accessor function", test => {
   test.equal(arr.mean([1, 2, 3]), 2);
   test.equal(arr.mean([{value: 1}, {value: 2}, {value: 3}], d => d.value), 2);
   test.equal(isNaN(arr.mean([{value: 1}, {value: 2}, {value: 3}])), true);
